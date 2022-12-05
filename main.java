8ee3c76ca2ac644a98a8bffa404463ba1833e849
zii.ayi <israh.zahid@ozu.edu.tr>
2022-12-05 03:43:11 +0300

    created a login ui using java swing

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
 public class main {
+<<<<<<< Updated upstream
     public main() {
         System.out.println("hello");
+=======
+    public static void main(String args[]) {
+        Login_UI.generate_login_ui();
+>>>>>>> Stashed changes
     }
 }
