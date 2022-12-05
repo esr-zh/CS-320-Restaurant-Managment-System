0fe36b2579d2f9e67d5ad19b6c77a00d649b0cf1
zii.ayi <israh.zahid@ozu.edu.tr>
2022-12-05 03:53:03 +0300

    resolved conflict

--- main.java
+++ main.java
@@ -1,10 +1,5 @@
 public class main {
-<<<<<<< Updated upstream
-    public main() {
-        System.out.println("hello");
-=======
     public static void main(String args[]) {
         Login_UI.generate_login_ui();
->>>>>>> Stashed changes
     }
 }
