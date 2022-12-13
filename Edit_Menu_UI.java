44f27c025dd371288d682cd53e69c053478a3064
zii.ayi <israh.zahid@ozu.edu.tr>
2022-12-13 17:17:52 +0300

    improved the design of employee ui and also center aligned the buttons using glue

--- Edit_Menu_UI.java
+++ Edit_Menu_UI.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class Edit_Menu_UI extends Login_UI implements ActionListener{
     // frame
@@ -13,6 +14,8 @@ public class Edit_Menu_UI extends Login_UI implements ActionListener{
     static JTable data_table;
 
     public static JButton editButton, deleteButton;
+
+    public static DefaultTableModel tableModel;
 
     public static void generate_table_ui() {
         {
@@ -30,6 +33,9 @@ public class Edit_Menu_UI extends Login_UI implements ActionListener{
             buttonPanel.setBorder(BorderFactory.createTitledBorder("Select and Manage Employees"));
             //panel.setLayout(null);
 
+            //tablePanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+            //buttonPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
             // Frame initialization
             employeeFrame = new JFrame();
             employeeFrame.setLayout(new BorderLayout());
@@ -42,34 +48,56 @@ public class Edit_Menu_UI extends Login_UI implements ActionListener{
             String[][] data = {
                     {"Drink", "Water", "$0.5"},
                     {"Dessert", "Cheesecake", "$5"},
+                    {"Main", "Cheeseburger", "4.99"},
+                    {"Drink", "Water", "$0.5"},
+                    {"Dessert", "Cheesecake", "$5"},
+                    {"Main", "Cheeseburger", "4.99"},
+                    {"Drink", "Water", "$0.5"},
+                    {"Dessert", "Cheesecake", "$5"},
+                    {"Main", "Cheeseburger", "4.99"},
+                    {"Drink", "Water", "$0.5"},
+                    {"Dessert", "Cheesecake", "$5"},
+                    {"Main", "Cheeseburger", "4.99"},
+                    {"Drink", "Water", "$0.5"},
+                    {"Dessert", "Cheesecake", "$5"},
+                    {"Main", "Cheeseburger", "4.99"},
                     {"Dessert", "Cheesecake", "$5"},
                     {"Dessert", "Cheesecake", "$5"},
                     {"Dessert", "Cheesecake", "$5"}
             };
-
             // Column Names
             String[] columnNames = {"Type", "Name", "Price"};
 
             // Initializing the JTable
-            data_table = new JTable(data, columnNames);
-            data_table.setBounds(30, 40, 200, 300);
+            tableModel = new DefaultTableModel(data, columnNames);
+            data_table = new JTable(tableModel);
+            data_table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); //prevent a user from selecting multiple rows
             // adding it to JScrollPane
             JScrollPane sp = new JScrollPane(data_table);
             tablePanel.add(sp);
 
+            buttonPanel.add(Box.createHorizontalGlue());
+
             editButton = new JButton("Edit");
             editButton.setForeground(Color.WHITE);
             editButton.setBackground(Color.BLACK);
+            editButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+            //editButton.setAlignmentY(Component.CENTER_ALIGNMENT);
             editButton.addActionListener((ActionListener) new Edit_Menu_UI());
-            editButton.setAlignmentY(Component.CENTER_ALIGNMENT);
             buttonPanel.add(editButton);
+
+            buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+            //buttonPanel.add(Box.createHorizontalGlue());
 
             deleteButton = new JButton("Delete");
             deleteButton.setForeground(Color.WHITE);
             deleteButton.setBackground(Color.BLACK);
+            deleteButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+            //deleteButton.setAlignmentY(Component.CENTER_ALIGNMENT);
             deleteButton.addActionListener((ActionListener) new Edit_Menu_UI());
-            deleteButton.setAlignmentY(Component.CENTER_ALIGNMENT);
             buttonPanel.add(deleteButton);
+
+            buttonPanel.add(Box.createHorizontalGlue());
 
             employeeFrame.add(tablePanel, BorderLayout.CENTER);
             employeeFrame.add(buttonPanel, BorderLayout.PAGE_END);
@@ -77,13 +105,16 @@ public class Edit_Menu_UI extends Login_UI implements ActionListener{
             employeeFrame.setVisible(true);
         }
     }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == editButton) {
             JOptionPane.showMessageDialog(null, "clicked Edit!");
-        } else {
-            JOptionPane.showMessageDialog(null, "Clicked delete!");
+        } else { //else user clicked delete
+            if(data_table.getSelectedRow() != -1) {
+                // remove selected row from the tableModel
+                tableModel.removeRow(data_table.getSelectedRow());
+                JOptionPane.showMessageDialog(null, "Selected employee deleted successfully");
+            }
         }
     }
 }
