ce2cb210821aed71fc4cc74155cfe7882e237163
mhd <mhdhd@gmail.com>
2022-12-13 18:06:10 +0300

    fix table in TransactionHistUI

--- Edit_Menu_UI.java
+++ Edit_Menu_UI.java
@@ -5,57 +5,122 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.border.Border;
-import javax.swing.border.LineBorder;
-import javax.swing.table.TableColumnModel;
+import javax.swing.table.DefaultTableModel;
 
 public class Edit_Menu_UI extends Login_UI implements ActionListener{
     // frame
-    static JFrame frame;
+    static JFrame employeeFrame;
     // Table
     static JTable data_table;
 
-    // Constructor
+    public static JButton editButton, deleteButton;
+
+    public static DefaultTableModel tableModel;
+
     public static void generate_table_ui() {
         {
+            //used boxLayout: reference: https://docs.oracle.com/javase/tutorial/uiswing/layout/box.html
+            //all layouts available at: https://docs.oracle.com/javase/tutorial/uiswing/layout/using.html
+            JPanel tablePanel = new JPanel();
+            JPanel buttonPanel = new JPanel();
+
+            BoxLayout layout1 = new BoxLayout(tablePanel, BoxLayout.PAGE_AXIS);
+            BoxLayout layout2 = new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS);
+            tablePanel.setLayout(layout1);
+            buttonPanel.setLayout(layout2);
+
+            tablePanel.setBorder(BorderFactory.createTitledBorder("Employee Table"));
+            buttonPanel.setBorder(BorderFactory.createTitledBorder("Select and Manage Employees"));
+            //panel.setLayout(null);
+
+            //tablePanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+            //buttonPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
             // Frame initialization
-            frame = new JFrame();
-
-            // Frame Title
-            frame.setTitle("Edit Menu");
-
+            employeeFrame = new JFrame();
+            employeeFrame.setLayout(new BorderLayout());
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            employeeFrame.setTitle("Edit Menu");
+            employeeFrame.setSize(700, 700);
+            employeeFrame.add(tablePanel);
+            employeeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             // Data to be displayed in the JTable
             String[][] data = {
-                    {"Drink", "Water", "$0.5", "Edit", "Delete"},
-                    {"Dessert", "Cheesecake", "$5", "Edit", "Delete"},
-                    {"Dessert", "Cheesecake", "$5", "Edit", "Delete"},
-                    {"Dessert", "Cheesecake", "$5", "Edit", "Delete"},
-                    {"Dessert", "Cheesecake", "$5", "Edit", "Delete"}
+                    {"Drink", "Water", "$0.5"},
+                    {"Dessert", "Cheesecake", "$5"},
+                    {"Main", "Cheeseburger", "4.99"},
+                    {"Drink", "Water", "$0.5"},
+                    {"Dessert", "Cheesecake", "$5"},
+                    {"Main", "Cheeseburger", "4.99"},
+                    {"Drink", "Water", "$0.5"},
+                    {"Dessert", "Cheesecake", "$5"},
+                    {"Main", "Cheeseburger", "4.99"},
+                    {"Drink", "Water", "$0.5"},
+                    {"Dessert", "Cheesecake", "$5"},
+                    {"Main", "Cheeseburger", "4.99"},
+                    {"Drink", "Water", "$0.5"},
+                    {"Dessert", "Cheesecake", "$5"},
+                    {"Main", "Cheeseburger", "4.99"},
+                    {"Dessert", "Cheesecake", "$5"},
+                    {"Dessert", "Cheesecake", "$5"},
+                    {"Dessert", "Cheesecake", "$5"}
             };
-
             // Column Names
-            String[] columnNames = {"Type", "Name", "Price", "", ""};
+            String[] columnNames = {"Type", "Name", "Price"};
 
             // Initializing the JTable
-            data_table = new JTable(data, columnNames);
-            data_table.setBounds(30, 40, 200, 300);
-
+            tableModel = new DefaultTableModel(data, columnNames);
+            data_table = new JTable(tableModel);
+            data_table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); //prevent a user from selecting multiple rows
             // adding it to JScrollPane
             JScrollPane sp = new JScrollPane(data_table);
-            frame.add(sp);
-            // Frame Size
-            frame.setSize(1000, 700);
-            // Frame Visible = true
-            frame.setVisible(true);
+            tablePanel.add(sp);
 
-            register_button = new JButton("Register");
-            register_button.setBounds(200, 110, 90, 25);
-            register_button.setForeground(Color.WHITE);
-            register_button.setBackground(Color.BLACK);
-            register_button.addActionListener((ActionListener) new Login_UI());
-            //panel.add(register_button);
-            frame.setVisible(true);
+            buttonPanel.add(Box.createHorizontalGlue());
+
+            editButton = new JButton("Edit");
+            btnProperties(editButton);
+            editButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+            //editButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+            editButton.addActionListener((ActionListener) new Edit_Menu_UI());
+            buttonPanel.add(editButton);
+
+            buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+            //buttonPanel.add(Box.createHorizontalGlue());
+
+            deleteButton = new JButton("Delete");
+            btnProperties(deleteButton);
+            deleteButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+            //deleteButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+            deleteButton.addActionListener((ActionListener) new Edit_Menu_UI());
+            buttonPanel.add(deleteButton);
+
+            buttonPanel.add(Box.createHorizontalGlue());
+
+            employeeFrame.add(tablePanel, BorderLayout.CENTER);
+            employeeFrame.add(buttonPanel, BorderLayout.PAGE_END);
+            employeeFrame.pack();
+            employeeFrame.setVisible(true);
         }
+    }
 
+    private static void btnProperties(JButton deleteButton) {
+        deleteButton.setForeground(Color.WHITE);
+        deleteButton.setBackground(Color.BLACK);
+        deleteButton.setOpaque(true);
+        deleteButton.setBorderPainted(false);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == editButton) {
+            JOptionPane.showMessageDialog(null, "clicked Edit!");
+        } else { //else user clicked delete
+            if(data_table.getSelectedRow() != -1) {
+                // remove selected row from the tableModel
+                tableModel.removeRow(data_table.getSelectedRow());
+                JOptionPane.showMessageDialog(null, "Selected employee deleted successfully");
+            }
+        }
     }
 }
--- UI/TransactionHistUI.java
+++ UI/TransactionHistUI.java
@@ -4,17 +4,20 @@ import javax.swing.*;
 
 public class TransactionHistUI extends JFrame {
 
-    JTable t;
+    JTable table;
 
-    String[]columns= {"Transaction ID","Name","Price","Quantity"};
-    String[][] cells= {{"12.12.2022","Pasta","500","12456"}};
+    String[]columns = {"Transaction ID","Name","Price","Quantity"};
+    String[][] cells= {
+            {"12.12.2022","Pasta","500","12456"}
+    };
 
     public TransactionHistUI() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        t = new JTable(cells,columns);
-        t.setBounds(200,200,200,200);
-        JScrollPane p = new JScrollPane(t);
-        this.add(p);
+        table = new JTable(cells,columns);
+        table.setBounds(200,200,200,200);
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setBorder(BorderFactory.createTitledBorder("Transaction History Table"));
+        this.add(scrollPane);
         this.setSize(800,800);
         this.setVisible(true);
     }
