69929ce36ba5fef6724980274a93e4e4c542b4ae
roshaan6678 <roshaan.rauf@gmail.com>
2022-12-16 07:17:54 +0300

    Created all the labels, Text fields and combo boxes to be used in the UI.

--- UI/OwnerMenu.java
+++ UI/OwnerMenu.java
@@ -6,16 +6,51 @@ import java.awt.event.ActionListener;
 
 
 public class OwnerMenu {
+    public static JPanel panel;
+    public static JFrame frame;
+    public static JLabel typeLabel, nameLabel, priceLabel, lb1;
+    public static JTextField nameText, priceText;
+
+
+
     public static void main(String[] args){
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         panel.setLayout(null);
-
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         frame.setTitle("Edit Menu");
-        frame.setSize(600,600);
+        frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        String[] type = {"Appetizer","Main Dish","Dessert","Drink"};
+
+        lb1 = new JLabel("Edit the following item your preference: ");
+        lb1.setBounds(5,10,300,20);
+        panel.add(lb1);
+
+        typeLabel = new JLabel("Item Type: ");
+        typeLabel.setBounds(100, 60,140, 20);
+        panel.add(typeLabel);
+        JComboBox<String> l = new JComboBox<>(type);
+        l.setBounds(200, 60, 140,25);
+        panel.add(l);
+
+        nameLabel = new JLabel("Item Name: ");
+        nameText =  new JTextField();
+        nameLabel.setBounds(100, 120,140, 20);
+        panel.add(nameLabel);
+        nameText.setBounds(200,120,140,25);
+        panel.add(nameText);
+
+        priceLabel = new JLabel("Item Price: ");
+        priceText = new JTextField();
+        priceLabel.setBounds(100, 180,140, 20);
+        panel.add(priceLabel);
+        priceText.setBounds(200,180,140,25);
+        panel.add(priceText);
+
         frame.setVisible(true);
-
-
     }
+
+
 }
