0c6f9751ee7a71fe3d0d7ee6d1804098b49ef26a
roshaan6678 <roshaan.rauf@gmail.com>
2022-12-16 18:19:12 +0300

    Added Item Quantity, Item description and also improved stylistic presence of text fields.

--- UI/OwnerMenu.java
+++ UI/OwnerMenu.java
@@ -1,15 +1,17 @@
 package UI;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class OwnerMenu implements ActionListener {
-    public static JPanel panel;
+class OwnerMenu implements ActionListener {
+    public static JPanel panel, p;
     public static JFrame frame;
-    public static JLabel typeLabel, nameLabel, priceLabel, lb1;
-    public static JTextField nameText, priceText;
+    public static JTextArea descText;
+    public static JLabel typeLabel, nameLabel, priceLabel, lb1, quantityLabel, descLabel;
+    public static JTextField nameText, priceText, quantityText;
     public static JButton button;
 
 
@@ -18,7 +20,7 @@ public class OwnerMenu implements ActionListener {
         panel.setLayout(null);
         frame = new JFrame();
         frame.setTitle("Edit Menu");
-        frame.setSize(500,500);
+        frame.setSize(500,600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
 
@@ -40,6 +42,7 @@ public class OwnerMenu implements ActionListener {
         nameLabel.setBounds(100, 120,140, 20);
         panel.add(nameLabel);
         nameText.setBounds(200,120,140,25);
+        nameText.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         panel.add(nameText);
 
         priceLabel = new JLabel("Item Price: ");
@@ -47,10 +50,28 @@ public class OwnerMenu implements ActionListener {
         priceLabel.setBounds(100, 180,140, 20);
         panel.add(priceLabel);
         priceText.setBounds(200,180,140,25);
+        priceText.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         panel.add(priceText);
 
+        quantityLabel = new JLabel("Item Quantity: ");
+        quantityText = new JTextField();
+        quantityLabel.setBounds(100, 240,140,20);
+        panel.add(quantityLabel);
+        quantityText.setBounds(200, 240,140,25);
+        quantityText.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        panel.add(quantityText);
+
+        descLabel = new JLabel("Item Description");
+        descText = new JTextArea();
+        descLabel.setBounds(100, 300,140,25);
+        panel.add(descLabel);
+        descText.setBounds(200, 300,140,100);
+        descText.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        panel.add(descText);
+
+
         button = new JButton("Confirm");
-        button.setBounds(180, 250,120,30);
+        button.setBounds(180, 450,120,30);
         button.setForeground(Color.WHITE);
         button.setBackground(Color.BLACK);
         button.setAlignmentX(Component.CENTER_ALIGNMENT);
