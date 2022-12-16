aed1bb2ddfcdcd36db82527eb4da4cd42fb71233
roshaan6678 <roshaan.rauf@gmail.com>
2022-12-16 04:43:16 +0300

    Set and made the Frame and Panel for the UI

--- UI/OwnerMenu.java
+++ UI/OwnerMenu.java
@@ -1,4 +1,21 @@
 package UI;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class OwnerMenu {
+    public static void main(String[] args){
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Edit Menu");
+        frame.setSize(600,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+    }
 }
