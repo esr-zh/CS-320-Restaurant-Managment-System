62835061e4535d8cb082a2ccf748de31fe6e6793
roshaan6678 <roshaan.rauf@gmail.com>
2022-12-19 01:39:08 +0300

    Refactored and cleaned the code by adding some classes and fixed some errors.

--- UI/OwnerMenu.java
+++ UI/OwnerMenu.java
@@ -8,13 +8,16 @@ import java.awt.event.ActionListener;
 
 
 public class OwnerMenu implements ActionListener {
+    public static int yCoordinate  = 60;
     public static JPanel panel;
     public static JFrame frame;
     public static JTextArea descText;
     public static JLabel typeLabel, nameLabel, priceLabel, lb1, quantityLabel, descLabel, portionLabel;
     public static JTextField nameText, priceText, quantityText, portionText;
     public static JButton button;
-    public static String name, price, quantity, portion, description;
+    public static JComboBox<String> l;
+    public static String typep, name, price, quantity, portion, description;
+    public static String[] type = {"Appetizer","Main Dish","Dessert","Drink"};
 
 
     public static void generateUI(){
@@ -26,68 +29,43 @@ public class OwnerMenu implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
 
-        String[] type = {"Appetizer","Main Dish","Dessert","Drink"};
 
-        lb1 = new JLabel("Edit the following item your preference: ");
+        lb1 = new JLabel("Edit the following item to your preference: ");
         lb1.setBounds(5,10,300,20);
         panel.add(lb1);
 
+        //Creating a drop-down menu
         typeLabel = new JLabel("Item Type: ");
-        typeLabel.setBounds(100, 60,140, 20);
+        typeLabel.setBounds(100, yCoordinate,140, 20);
         panel.add(typeLabel);
-        JComboBox<String> l = new JComboBox<>(type);
-        l.setBounds(200, 60, 140,25);
+        l = new JComboBox<>(type);
+        l.setBounds(200, yCoordinate, 140,25);
         panel.add(l);
+        yCoordinate+=60;
 
         nameLabel = new JLabel("Item Name: ");
         nameText =  new JTextField();
-        nameLabel.setBounds(100, 120,140, 20);
-        panel.add(nameLabel);
-        nameText.setBounds(200,120,140,25);
-        nameText.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panel.add(nameText);
+        addToPanel(nameLabel, nameText);
 
         priceLabel = new JLabel("Item Price: ");
         priceText = new JTextField();
-        priceLabel.setBounds(100, 180,140, 20);
-        panel.add(priceLabel);
-        priceText.setBounds(200,180,140,25);
-        priceText.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panel.add(priceText);
+        addToPanel(priceLabel, priceText);
 
         quantityLabel = new JLabel("Item Quantity: ");
         quantityText = new JTextField();
-        quantityLabel.setBounds(100, 240,140,20);
-        panel.add(quantityLabel);
-        quantityText.setBounds(200, 240,140,25);
-        quantityText.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panel.add(quantityText);
+        addToPanel(quantityLabel,quantityText);
 
         portionLabel = new JLabel("Item Portion:");
         portionText = new JTextField();
-        portionLabel.setBounds(100, 300,140,20);
-        panel.add(portionLabel);
-        portionText.setBounds(200, 300,140,25);
-        portionText.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panel.add(portionText);
+        addToPanel(portionLabel,portionText);
 
+        //Creating a Text Area big enough for description
         descLabel = new JLabel("Item Description:");
         descText = new JTextArea();
-        descLabel.setBounds(100, 360,140,20);
-        panel.add(descLabel);
-        descText.setBounds(200, 360,140,80);
-        descText.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panel.add(descText);
-
+        addTextArea(descLabel, descText);
 
         button = new JButton("Confirm");
-        button.setBounds(180, 480,120,30);
-        button.setForeground(Color.WHITE);
-        button.setBackground(Color.BLACK);
-        button.setAlignmentX(Component.CENTER_ALIGNMENT);
-        button.setOpaque(true);
-        button.addActionListener(new OwnerMenu());
-        panel.add(button);
+        btnProperties(button);
 
         frame.setVisible(true);
     }
@@ -96,13 +74,45 @@ public class OwnerMenu implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button) {
             JOptionPane.showMessageDialog(null, "Changes to item have been saved!");
+            typep  = (String) l.getSelectedItem();
             name = nameText.getText();
             price = priceText.getText();
             quantity = quantityText.getText();
             portion = portionText.getText();
             description = descText.getText();
-            System.out.println(name + "\n" + price + "\n" + quantity +
+            System.out.println(typep + "\n" + name + "\n" + price + "\n" + quantity +
                     "\n" + portion + "\n" + description);
         }
     }
+
+    public static void addToPanel(JLabel label, JTextField text){
+        label.setBounds(100, yCoordinate,140, 20);
+        panel.add(label);
+        text.setBounds(200, yCoordinate,140,25);
+        text.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        panel.add(text);
+        yCoordinate+=60;
+    }
+
+    public static void btnProperties(JButton b){
+        b.setBounds(180, yCoordinate+60,120,30);
+        b.setForeground(Color.WHITE);
+        b.setBackground(Color.BLACK);
+        b.setAlignmentX(Component.CENTER_ALIGNMENT);
+        b.setOpaque(true);
+        b.addActionListener(new OwnerMenu());
+        panel.add(b);
+    }
+
+    public static void addTextArea(JLabel label, JTextArea Atext){
+        label.setBounds(100, yCoordinate,140,20);
+        panel.add(descLabel);
+        Atext.setBounds(200, yCoordinate,140,80);
+        Atext.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        Atext.setLineWrap(true);
+        Atext.setWrapStyleWord(true);
+        panel.add(Atext);
+        yCoordinate+=60;
+    }
+
 }
