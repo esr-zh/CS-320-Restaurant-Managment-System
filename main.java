87a19ef121f597e0be5b7272a5482eba1b5cd221
roshaan6678 <roshaan.rauf@gmail.com>
2022-12-19 04:08:49 +0300

    Made an Edit Menu UI where an owner can manage the items in their menu.

--- main.java
+++ main.java
@@ -1,8 +1,10 @@
+import UI.EditMenu;
 import UI.OwnerMenu;
 
 public class main {
     public static void main(String args[]) {
         Login_UI.generate_login_ui();
         OwnerMenu.generateUI();
+        EditMenu.generateUI();
     }
 }
