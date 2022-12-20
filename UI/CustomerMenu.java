0fe3fc1b5435edbada9161520c69c86242b995ac
Ege Seçkin <ege.seckin@ozu.edu.tr>
2022-12-21 01:02:29 +0300

    Products get seperated by menu type, price of the product can be seen now

--- UI/CustomerMenu.java
+++ UI/CustomerMenu.java
@@ -31,18 +31,14 @@ public class CustomerMenu {
         JLabel cart = new JLabel("Currently in cart");
         subBotPanelW.add(cart, BorderLayout.PAGE_START);
 
-        String[][] data = {};
-        String[] columnNames = {"Type", "Name", "Price"};
-        JTable itemTable = new JTable(data,columnNames);
-        subBotPanelW.add(itemTable, BorderLayout.CENTER);
 
         JPanel subBotPanelE = new JPanel();
         botPanel.add(subBotPanelE, BorderLayout.EAST);
         subBotPanelE.setLayout(new BorderLayout());
 
         JButton addButton = new JButton("Add to cart");
-        addButton.addActionListener(new Add());
         subBotPanelE.add(addButton, BorderLayout.NORTH);
+
 
         JButton delButton = new JButton("Delete from cart");
         delButton.addActionListener(new Delete());
@@ -56,25 +52,32 @@ public class CustomerMenu {
         String[] menuTypes ={"Appetizer","Main Dish","Dessert","Drink"};
         JComboBox menuTypeBar = new JComboBox(menuTypes);
 
-
-        String getMenuType = menuTypeBar.getSelectedItem().toString();
-
-
-        String[] products = new String[10];
+        String[] products =  new String[1];
+        products[0]="Soup";
         JComboBox productsList = new JComboBox(products);
-        if(getMenuType.equals("Appetizer")){
-            productsList.removeAllItems();
-            productsList.addItem("Soup");
-        } else if (getMenuType.equals("Main Dish")) {
-            productsList.removeAllItems();
-            productsList.addItem("Cheeseburger");
-        } else if (getMenuType.equals("Dessert")) {
-            productsList.removeAllItems();
-            productsList.addItem("Cheesecake");
-        } else if (getMenuType.equals("Drink")) {
-            productsList.removeAllItems();
-            productsList.addItem("Water");
-        }
+        menuTypeBar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String getMenuType = menuTypeBar.getSelectedItem().toString();
+                if(menuTypeBar.getSelectedItem().toString().equals("Appetizer")){
+                    productsList.removeAllItems();
+                    productsList.addItem("Soup");
+                }else  if (menuTypeBar.getSelectedItem().toString().equals("Main Dish")) {
+                    productsList.removeAllItems();
+                    productsList.addItem("Cheeseburger");
+                }else if (getMenuType.equals("Dessert")) {
+                    productsList.removeAllItems();
+                    productsList.addItem("Cheesecake");
+                }else  if (getMenuType.equals("Drink")) {
+                    productsList.removeAllItems();
+                    productsList.addItem("Water");
+                }
+            }
+        });
+
+
+
+
 
 
          String[] quantities = new String[11];
@@ -84,19 +87,67 @@ public class CustomerMenu {
          JComboBox quantityBar = new JComboBox(quantities);
 
 
-        String getFoodPrice = menuTypeBar.getSelectedItem().toString();
-         
-         
-        String price = "$3";
-         if(getFoodPrice.equals("Soup")){
-            price= "$3";
-        } else if (getFoodPrice.equals("Cheeseburger")) {
-            price="$4.99";
-        } else if (getFoodPrice.equals("Cheesecake")) {
-            price="$5";
-        } else if (getFoodPrice.equals("Water")) {
-            price="$0.5";
-        }
+        JLabel priceLabel = new JLabel();
+
+         productsList.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 if(productsList.getSelectedItem()!=null){
+                     String getFoodPrice = productsList.getSelectedItem().toString();
+                     if(getFoodPrice.equals("Soup")){
+                         priceLabel.setText("$3");
+                     } else if (getFoodPrice.equals("Cheeseburger")) {
+                         priceLabel.setText("$4.99");
+                     } else if (getFoodPrice.equals("Cheesecake")) {
+                         priceLabel.setText("$5");
+                     } else if (getFoodPrice.equals("Water")) {
+                         priceLabel.setText("$0.5");
+                     }
+                 }else{
+                 priceLabel.setText("0");
+                 }
+             }
+
+
+
+         });
+
+
+        String[][] data = new String[10][10];
+        String[] columnNames = {"Type", "Name", "Price"};
+        JTable itemTable = new JTable(data,columnNames);
+        subBotPanelW.add(itemTable, BorderLayout.CENTER);
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (productsList.getSelectedItem() != null) {
+                    String getTypeName = menuTypeBar.getSelectedItem().toString();
+                    String getProductName = productsList.getSelectedItem().toString();
+                    String getFoodPrice2 = priceLabel.getText();
+                    String getQuantity = quantityBar.getSelectedItem().toString();
+                    int x =15;
+                    switch (getProductName) {
+                        case"Soup":
+                            x=0;
+                            break;
+                        case "Cheeseburger":
+                            x=1;
+                            break;
+                        case "Cheesecake":
+                            x=2;
+                            break;
+                        case "Water":
+                            x=3;
+                            break;
+                    }
+                    data[x][0] = getTypeName;
+                    data[x][1] = getProductName;
+                    data[x][2] = getFoodPrice2 + "*" + getQuantity;
+
+                }
+            }
+        });
          
 
          
@@ -107,7 +158,7 @@ public class CustomerMenu {
         midPanel.add(new MenuLabelPart("Quantity:"));
         midPanel.add(quantityBar);
         midPanel.add(new MenuLabelPart("Price of product"));
-        midPanel.add(new MenuLabelPart(price));
+        midPanel.add((priceLabel));
 
 
 
