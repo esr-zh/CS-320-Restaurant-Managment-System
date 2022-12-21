71629d839bb0e4bc318335e73288b7fb562e1a75
mhd <mhdhd@gmail.com>
2022-12-21 12:59:51 +0300

    set look and feel the same class

--- main.java
+++ main.java
@@ -1,8 +1,14 @@
 import UI.*;
+import javax.swing.*;
 
 public class main {
     public static void main(String args[]) {
         //loginUI.generate_login_ui();
-        CustomerMenuUI.generateCustomerUI();
+        try {
+            UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
+            CustomerMenuUI.generateCustomerUI();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
